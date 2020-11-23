b549e81d1650c5661521d147146edaa5c07d0713
aldoserehd <68321828+aldoserehd@users.noreply.github.com>
2020-11-23 17:01:31 +0300

    Done

---
+++ books.js
@@ -0,0 +1,39 @@
+const books = [
+  {
+    id: 1,
+    name: "ال سعود في رحاب الكويت",
+    slug: "ال-سعود-في-رحاب-الكويت",
+    description:
+      "جاءت موضوعات الكتاب بأسلوب شيق ولغة سهلة، و«عامية» في بعض الأوقات، معززة بالصور الخالدة والثمينة ومبنية على حقائق تاريخية، إذ استمد المؤلف معظم نصوص الكتاب من وثائق ومخطوطات قديمة ظلت بين أيد أمينة حتى يومنا هذا، وكذلك من مخططات هندسية حرص الكاتب على الوصول إليها بين أروقة مؤسسات حكومية كويتية تكشف عن جانب بالغ الأهمية من حياة الأسرة المالكة السعودية في الكويت.",
+    price: 20,
+    image: "https://pbs.twimg.com/media/EfE6a7RWoAEJSD5?format=jpg&name=large",
+  },
+  {
+    id: 2,
+    name: "رجال من التاريخ",
+    slug: "رجال-من-التاريخ",
+    description: "رجال من التاريخ كتاب للعلامة السوري الدمشقي علي الطنطاوي",
+    price: 15,
+    image:
+      "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1512766220l/3859307.jpg",
+  },
+  {
+    id: 3,
+    name: "فبهداهم اقتده",
+    slug: "فبهداهم-اقتده",
+    description: "سير وقصص الأنبياء عليهم السلام رابط المادة",
+    price: 23,
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcT-bNFaFQMAA1qU5M4wLNADpsy3RCXt2IWRQg&usqp=CAU",
+  },
+  {
+    id: 4,
+    name: "إلى الجيل الصاعد",
+    slug: "إلى-الجيل-الصاعد",
+    description: "احمد بن يوسف السيد",
+    price: 12,
+    image:
+      "https://www.nourbook.com/publice/covers_cache_webp/2/f/b/7/377743f627fb773cb1308dafe6cbc906.jpg.webp",
+  },
+];
+module.exports = books;
